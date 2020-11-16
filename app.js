8c326170d01e8629434f86968c8ef013601f38ad
unknown <amitesh007ranjan@gmail.com>
2020-11-16 13:14:35 +1100

    init commit

---
+++ app.js
@@ -0,0 +1,84 @@
+// Game Values
+ let min = 1,
+     max = 10,
+     winningNum = getRandomNum(min, max),
+     guessesLeft = 3;
+
+
+// UI Elements
+
+const game = document.querySelector('#game'),
+      minNum = document.querySelector('.min-num'),
+      maxNum = document.querySelector('.max-num'),
+      guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
+      message = document.querySelector('.message');
+
+// Assign UI min and max
+
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for play again
+
+game.addEventListener('mousedown', function(e){
+    if(e.target.className === 'play-again'){
+        window.location.reload();
+    }
+    
+});
+
+// Listen for guess
+
+guessBtn.addEventListener('click', function(){
+    let guess = parseInt(guessInput.value);
+
+    // Validate the input
+    if(isNaN(guess) || guess < min || guess > max){
+        setMessage(`Please enter a number between ${min} and
+        ${max}`, 'red');
+    }else{
+
+        // Check if won
+    if(guess === winningNum){
+        gameOver(true, `Congratulation! you won, ${guess} is correct` );
+        
+    }else{
+        // Wrong guess
+        guessesLeft -= 1;
+        if (guessesLeft === 0){
+        gameOver(false,`You lost, ${winningNum} was  correct number`);
+        }
+        else{
+
+            guessInput.value = '';
+            setMessage(`${guess} is not correct, ${guessesLeft} 
+            guesses left`, 'red');
+        }
+
+    }
+
+    }
+ 
+});
+
+function gameOver(won, msg){
+    let color;
+    won === true ? color = 'green' : color = 'red';
+    guessInput.disabled = true;
+    guessInput.style.borderColor = color;
+    setMessage(msg, color);
+    
+    // Play again ?
+    guessBtn.value = 'Play Again';
+    guessBtn.className += 'play-again';
+}
+
+function getRandomNum(min, max){
+    return (Math.floor(Math.random()*(max-min+1)+min));
+}
+
+function setMessage(msg, color){
+    message.textContent = msg;
+    message.style.color = color;
+}
